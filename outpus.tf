aba62c78c2289b8f8059d9bfa849101ee2546887
oncallejas <oncallejas@stefanini.com>
2022-05-03 22:27:45 -0600

    feat: agregando mas informacion a las variables de output

--- outpus.tf
+++ outpus.tf
@@ -1,3 +1,19 @@
-output "ip" {
+output "private-ip" {
   value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+
+output "vm-name" {
+  value = google_compute_instance.vm_instance.name
+}
+
+output "network-name" {
+  value = google_compute_network.vpc_network.name
+}
+
+output "subnetwork-name" {
+  value = google_compute_subnetwork.subnetwork.name
+}
+
+output "public-ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
 }
